385fbd5eec246a9986c8681118bbb1fc78b0450f
a32871004 <a2215939>
2019-10-30 18:07:45 +0800

    M2Q11

--- src/w804.java
+++ src/w804.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class w804 {
     public static void main(String[] args) {
-
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        for(int i=1;i<=n;i++){
+            if (n%i==0){
+                System.out.println(i);
+            }
+        }
     }
 }
